5829e3ae68065470e71465ac94ff075607818b13
Dezz-mond <desmonomale@gmail.com>
2026-09-11 12:10:06 +0000

    First assignment with BeTechified

---
+++ intro.js
@@ -0,0 +1,9 @@
+const user = {
+    name: "Desmond Ndefo",
+    age: 21,
+    favoriteProgrammingLanguage: "Python"
+};
+
+console.log("Name:", user.name);
+console.log("Age:", user.age);
+console.log("Favorite Programming Language:", user.favoriteProgrammingLanguage);
